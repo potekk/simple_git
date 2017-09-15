fbb8e17c48f47d3ee571e834621fbeeeaf650f96
Piotr Skoroszewski <piotr.skoroszewski@gmail.com>
2017-09-15 18:12:13 +0200

    Adding elements to staging

--- simple_git/simple_git.py
+++ simple_git/simple_git.py
@@ -12,6 +12,7 @@ class SimpleGit:
     SGIT_DIR = '.sgit'
     SGIT_OBJECTS_DIR = 'objects'
     SGIT_HEAD = 'HEAD'
+    SGIT_STAG = 'STAG'
     SGIT_FILES_DIR = 'objects/files'
     SGIT_ROOT_NODE = 'root_node'
 
@@ -24,16 +25,32 @@ class SimpleGit:
         :param args:
         :return:
         """
-        self._assert_init_is_possible()
+        #self._assert_init_is_possible()
         self._create_init_dir()
 
     def add_cmd(self, args):
-        print 'add files', args
+        """
+
+        :param args:
+        :return:
+        """
+        files_abs = [os.path.join(self.SGIT_ROOT_DIR, f) for f in args.files]
+        self.add_files_to_stag(files_abs)
+        print files_abs
 
     def commit_cmd(self, args):
-        print 'commit'
+        """
+
+        :param args:
+        :return:
+        """
 
     def status_cmd(self, args):
+        """
+
+        :param args:
+        :return:
+        """
         print 'status'
 
     def _assert_init_is_possible(self):
@@ -57,14 +74,40 @@ class SimpleGit:
         sgit_root_node_file = os.path.join(sgit_objects_dir_abs, self.SGIT_ROOT_NODE)
         self._create_tree_node(filename=sgit_root_node_file, parent=None, files_meta=[])
 
+        # STAG file
+        sgit_stag_file = os.path.join(sgit_dir_abs, self.SGIT_STAG)
+        self._create_tree_node(filename=sgit_stag_file, parent=sgit_root_node_file, files_meta=[])
+
         # HEAD file
         sgit_head_file = os.path.join(sgit_dir_abs, self.SGIT_HEAD)
         self._move_head_pointer(sgit_head_file, sgit_root_node_file)
 
+    def _get_stag_filename(self):
+        return os.path.join(self.SGIT_ROOT_DIR, self.SGIT_DIR, self.SGIT_STAG)
+
     def _create_tree_node(self, filename, parent, files_meta):
         with open(filename, 'w') as tn_fd:
-            tn_fd.write(TreeNode(parent, files_meta).to_json())
+            tn_fd.write(TreeNode(parent, files_meta).dumps())
 
     def _move_head_pointer(self, sgit_head_file, tree_node_file_to_point):
         with open(sgit_head_file, 'w') as head_fd:
             head_fd.write(tree_node_file_to_point)
+
+    def add_files_to_stag(self, files):
+        stag_filename = self._get_stag_filename()
+        stag_node = TreeNode.load(stag_filename)
+        self._add_files_rec(files, stag_node)
+        stag_node.dump(stag_filename)
+
+    def _add_files_rec(self, files, tree_node):
+        files_to_add = []
+        for f in files:
+            if os.path.isdir(f):
+                for base_dir, _, dir_files in os.walk(f):
+                    for dir_file in dir_files:
+                        new_file_abs = os.path.join(base_dir, dir_file)
+                        files_to_add.append(new_file_abs)
+            else:
+                files_to_add.append(f)
+
+        map(lambda x: tree_node.add_file(x), files_to_add)
--- simple_git/tree_node.py
+++ simple_git/tree_node.py
@@ -1,19 +1,108 @@
+import gzip
 import json
+
+import os
+
+import shutil
+
+from .utils import sha1_file
+
+
+class FileState:
+    """
+    Enum for file states
+    """
+    NEW = 1
+    MODIFIED = 2
+    THE_SAME = 3
 
 
 class TreeNode:
-    def __init__(self, parent, files_meta):
-        self.parent = parent
-        self.files_meta = files_meta
+    IGNORE_DIRS = ['.sgit']
 
-    def to_json(self):
+    def __init__(self, parent=None, files_meta=None):
+        self.parent = parent
+        self.files_meta = files_meta or {}
+
+    def create_file_meta(self, filename, file_hash, fobj_filename):
+        file_meta = {
+            'filename': filename,
+            'hash': file_hash,
+            'fobj_filename': fobj_filename,
+        }
+        return file_meta
+
+    def dump(self, filename):
         me_json = {
             'parent': self.parent,
             'files_meta': self.files_meta
         }
-        return json.dumps(me_json)
+        with open(filename, 'w') as fd:
+            json.dump(me_json, fd, indent=2)
 
-    def from_json(self, text):
+    def dumps(self):
+        me_json = {
+            'parent': self.parent,
+            'files_meta': self.files_meta
+        }
+        return json.dumps(me_json, indent=2)
+
+    @staticmethod
+    def loads(text):
         me_json = json.loads(text)
-        self.parent = me_json['parent']
-        self.files_meta = me_json['files_meta']
+        parent = me_json['parent']
+        files_meta = me_json['files_meta']
+        return TreeNode(parent, files_meta)
+
+    @staticmethod
+    def load(filename):
+        with open(filename) as f:
+            return TreeNode.loads(f.read())
+
+    def determine_files_changes(self, prev_node, root_directory):
+        file_states = {}
+        for base_dir, _, filename_list in os.walk(root_directory):
+            for filename in filename_list:
+                filename_abs = os.path.join(base_dir, filename)
+                file_states['filename'] = self.get_file_state(prev_node, filename_abs)
+
+        return file_states
+
+    @staticmethod
+    def get_file_state(prev_node, filename_abs):
+        if filename_abs not in prev_node.file_meta:
+            return FileState.NEW
+        prev_file_meta = prev_node.file_meta['filename_abs']
+        file_hash = sha1_file(filename_abs)
+        if prev_file_meta['hash'] == file_hash:
+            return FileState.THE_SAME
+        else:
+            return FileState.MODIFIED
+
+    def add_file(self, filename):
+        file_hash = sha1_file(filename)
+        if filename not in self.files_meta:
+            self.create_new_file_obj(file_hash, filename)
+        else:
+            prev_file_meta = self.files_meta[filename]
+            if prev_file_meta['hash'] == file_hash:
+                pass  # The files match
+            else:
+                os.unlink(prev_file_meta['fobj_filename'])
+                self.create_new_file_obj(file_hash, filename)
+
+    def create_new_file_obj(self, file_hash, filename, obj_path):
+        stored_file = self.store_file(filename, file_hash, obj_path)
+        file_meta = self.create_file_meta(filename, file_hash, stored_file)
+        self.update_files_meta(filename, file_meta)
+
+    def store_file(self, filename, file_hash, obj_path):
+        base_dir = obj_path
+        out_filename_abs = os.path.join(base_dir, file_hash)
+        with gzip.open(out_filename_abs, 'w') as f_out:
+            with open(filename) as f_in:
+                shutil.copyfileobj(f_in, f_out)
+        return out_filename_abs
+
+    def update_files_meta(self, filename, file_meta):
+        self.files_meta[filename] = file_meta
