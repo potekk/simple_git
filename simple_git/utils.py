1753f88545c1064da6bd38d3adb7b55b7c4092ec
Piotr Skoroszewski <piotr.skoroszewski@gmail.com>
2017-09-15 13:45:46 +0200

    Add hash functions

--- simple_git/utils.py
+++ simple_git/utils.py
@@ -1,5 +1,19 @@
 import os
+import hashlib
 
 
 def check_file_exists(path):
     return os.path.exists(path)
+
+
+def sha1_str(input_str):
+    m = hashlib.sha1()
+    m.update(input_str)
+    return m.hexdigest()
+
+def sha1_file(input_filename):
+    m = hashlib.sha1()
+    with open(input_filename, "rb") as in_fd:
+        for chunk in iter(lambda: in_fd.read(4096), b""):
+            m.update(chunk)
+    return m.hexdigest()
